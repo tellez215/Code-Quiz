f8a99c8119625b9d28e0b68ceffdc73e719afdec
CT <tellezc007@gmail.com>
2022-04-05 17:25:08 -0700

    add questions and answers and make buttons hidden

--- assests/code.js
+++ assests/code.js
@@ -1,25 +1,34 @@
 const startButton = document.getElementById('startbtn')
-const questionContainerEl = document.getElementById('questioncon')
-
+const nextButton = document.getElementById('nextbtn')
+const questionConEl = document.getElementById('questioncon')
 const questionEl = document.getElementById('questions')
 const answerBtnEl = document.getElementById('answerbtn')
+const scoreTracker = document.getElementById('scoreTracker')
+const scoreUpEl = document.getElementById('scoreUp')
 
 let randomQuestions, currentQuestion
 
 startButton.addEventListener('click', startGame)
-
+nextButton.addEventListener('click', () => {
+    currentQuestion++
+    setNextQuestion()
+})
 
 function startGame() {
     console.log('started!')
     startButton.classList.add('hide')
     randomQuestions = questions.sort(() => Math.random() - .5)
     currentQuestion = 0
-    questionContainerEl.classList.remove('hide')
+    questionConEl.classList.remove('hide')
     setNextQuestion()
+    scoreUpEl.textContent = 0;
 }
 
 function setNextQuestion() {
+
+    resetAll()
     showQuestion(randomQuestions[currentQuestion])
+
 }
 
 function showQuestion(question) {
@@ -36,8 +45,40 @@ function showQuestion(question) {
     })
 }
 
-function selectAnswer(e) {
+function resetAll() {
+    nextButton.classList.add('hide')
+    while (answerBtnEl.firstChild) {
+        answerBtnEl.removeChild(answerBtnEl.firstChild)
+    }
+}
 
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerBtnEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (randomQuestions.lenght > currentQuestion + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'restart'
+        startButton.classList.remove('hide')
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 
